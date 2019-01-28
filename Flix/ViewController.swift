29a9aa6c098ab5fd2c65a408a3679b02cc1b9f13
Sampson Liao <sampsonliao@sampsons-mac.local>
2019-01-28 14:21:33 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Sampson Liao on 1/28/19.
+//  Copyright © 2019 sliao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
