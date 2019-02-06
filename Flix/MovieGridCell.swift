f356b0ebe697d18f7602deaa446c82c2e70ce9c1
Sampson Liao <sampsonliao@Sampsons-Mac.local>
2019-02-06 10:47:04 -0800

    implemented collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sampson Liao on 2/6/19.
+//  Copyright © 2019 sliao. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MovieGridViewController: UIViewController {
-
-    var movies = [[String:Any]]()
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    var movies = [[String:Any]]()
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
+
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -26,7 +48,7 @@ class MovieGridViewController: UIViewController {
                 print(error.localizedDescription)
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                
+                self.collectionView.reloadData()
                 self.movies = dataDictionary["results"] as! [[String: Any]]
                 
                 print(self.movies)
